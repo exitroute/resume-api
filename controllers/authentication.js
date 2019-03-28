fb38f23884c72d135169f58adf39ea20b78a8308
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-03-28 17:45:12 +0100

    WIP authentication controller simplified

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -5,74 +5,82 @@ const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 
 const app = require("../app");
-const User = require("../model/User");
+const Contact = require("../model/Contact");
 
 exports.authenticate = async function (req, res) {
   try {
-    const user = await User.findOne({ name: req.body.name });
-    console.log("user", user);
 
-    if (!user) {
-      res.json({
-        success: false,
-        message: "Authentication failed. User not found."
-      });
-    } else {
-      const hashedPW = bcrypt.compareSync(req.body.password, user.password); // true
+    console.log("authentication request for", req.body.name);
+    console.log("token", req.headers.authorization)
+    // const user = await User.findOne({ name: req.body.name });
+    // console.log("user", user);
 
-      if (!hashedPW) {
-        return res.json({
-          success: false,
-          message: "Authentication failed. Wrong token."
-        });
-      } else {
-        const payload = {
-          user: user.name,
-          admin: user.admin
-        };
-        const token = jwt.sign(payload, app.get("superSecret"), {
-          expiresIn: 86400
-        });
+    // if (!user) {
+    //   res.json({
+    //     success: false,
+    //     message: "Authentication failed. User not found."
+    //   });
+    // } else {
+    //   const hashedPW = bcrypt.compareSync(req.body.password, user.password); // true
 
-        return res.json({
-          success: true,
-          status: 200,
-          user: user,
-          message: "Enjoy your token!",
-          token: token
-        });
-      }
-    }
+    //   if (!hashedPW) {
+    //     return res.json({
+    //       success: false,
+    //       message: "Authentication failed. Wrong token."
+    //     });
+    //   } else {
+    //     const payload = {
+    //       user: user.name,
+    //       admin: user.admin
+    //     };
+    //     const token = jwt.sign(payload, app.get("superSecret"), {
+    //       expiresIn: 86400
+    //     });
+
+    //     return res.json({
+    //       success: true,
+    //       status: 200,
+    //       user: user,
+    //       message: "Enjoy your token!",
+    //       token: token
+    //     });
+    //   }
+    // }
   } catch (error) {
     console.log(error);
   }
 };
 
-exports.newUser = async (req, res) => {
+exports.newContact = async (req, res) => {
   if (/\S+@\S+\.\S+/.test(req.body.name)) {
     try {
-      const user = await User.findOne({ name: req.body.name });
+      console.log("new contact request", req.body.name)
+      // const contact = await Contact.findOne({ name: req.body.name });
 
-      if (user) {
-        res.json({
-          success: false,
-          status: 409,
-          message: "User already exists!"
-        });
-      } else if (!user) {
-        const hash = bcrypt.hashSync(req.body.password, salt);
-        const userToken = uuidv4(4);
-        const user = new User({
-          name: req.body.name,
-          password: hash,
-          admin: false,
-          verificationToken: userToken
-        });
-        await user.save();
-        console.log("user saved")
-      }
+      // if (contact) {
+      //   res.json({
+      //     success: false,
+      //     status: 409,
+      //     message: "contact already exists!"
+      //   });
+      // } else if (!contact) {
+      //   const hash = bcrypt.hashSync(req.body.password, salt);
+      //   const contactToken = uuidv4(4);
+      //   const contact = new Contact({
+      //     name: req.body.name,
+      //     password: hash,
+      //     admin: false,
+      //     verificationToken: contactToken
+      //   });
+      //   await contact.save();
+      //   console.log("contact saved")
+      // }
     } catch (error) {
       console.log(error);
     }
   }
 }
+
+exports.allContacts = async (req, res) => {
+  console.log("all contacts returned");
+} 
