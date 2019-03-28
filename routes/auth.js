0bc8856d2add3a6334ebd0e7ccf954cd8e9b0c81
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-03-28 17:42:37 +0100

    routes added

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { authMessage } = require("../controllers/messages");
-const { authenticate, newUser } = require("../controllers/authentication");
+const { authenticate, newContact, allContacts } = require("../controllers/authentication");
 
 router
   .route("/")
   .get(authMessage)
   .post(authenticate);
+
+router
+  .route("/contacts")
+  .get(allContacts)
+  .post(newContact);
 
 module.exports = router;
