ab78e9c3aa5ae0a667b2d7aa595436e667e93ed0
Ryan O'Shea <ryan.oshea@exitroute.org>
2019-07-09 23:27:39 +0200

    formatting

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const logger = require('morgan');
+const logger = require("morgan");
 const cors = require("cors");
 
 require("dotenv").config({ path: "./.env" });
 
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-mongoose.connect(
-  process.env.DB_URL,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  }
-);
-mongoose.connection.on("error", console.error)
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+mongoose.connection.on("error", console.error);
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.set("superSecret", "thisShould be supersecret")
+app.use(logger("dev"));
+app.set("superSecret", "thisShould be supersecret");
 
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
 
 const contactsRouter = require("./routes/contacts");
 app.use("/contacts", contactsRouter);
 
-const authRouter = require('./routes/auth');
-app.use('/auth', authRouter);
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
 
 console.log(`server is running on ${port}`);
 
